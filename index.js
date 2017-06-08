a09632aacf3b489b34d140bbd3fd442a42389419
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-06-08 16:24:30 -0400

    Updating codebase to include basic Kinesis handler

---
+++ index.js
@@ -0,0 +1,19 @@
+/* eslint-disable semi */
+const processRecord = (record, callback) => {
+  return Buffer.from(record.kinesis.data, 'base64').toString('utf8');
+};
+
+const kinesisStreamHandler = (records, context) => {
+  const data = records.map(record => processRecord(record));
+  return data;
+};
+
+exports.handler = (event, context, callback) => {
+  const record = event.Records[0] || {};
+
+  // Handle Kinesis Stream
+  if (record.kinesis && record.kinesis.data) {
+    const kinesisData = kinesisStreamHandler(event.Records, context);
+    console.log('Kinesis Data', kinesisData, typeof kinesisData);
+  }
+};
