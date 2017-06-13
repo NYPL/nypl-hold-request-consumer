ce071dcc4dc548e28793fee0247d870baed15151
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-06-13 15:21:15 -0400

    Updated repo to include unit tests using Mocha/Chai/Sinon. Added npm script to run unit-tests.

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 /* eslint-disable semi */
 const NyplStreamsClient = require('@nypl/nypl-streams-client');
 
+exports.kinesisHandler = (records, opts = {}, context) => {
+  // TODO: Add Error Handling Class
+  if (!opts.schema || opts.schema === '') {
+    return null;
+  }
+  if (!opts.apiUri) {
+    return null;
+  }
+
+  const schema = opts.schema;
+  const apiUri = opts.apiUri;
+  const streamsClient = new NyplStreamsClient({ nyplDataApiClientBase: apiUri });
+  const decodedKinesisData = streamsClient.decodeData(schema, records.map(i => i.kinesis.data));
+
+  // Resolve the Promise and do something with the decoded data
+  return decodedKinesisData
+    .then((result) => console.log('result:', result))
+    .catch((err) => console.log('rejected:', err));
+};
+
 exports.handler = (event, context, callback) => {
   const record = event.Records[0] || {};
-  const streamsClient = new NyplStreamsClient({ nyplDataApiClientBase: 'https://api.nypltech.org/api/v0.1/' });
   // Handle Kinesis Stream
   if (record.kinesis && record.kinesis.data) {
-    const decodedKinesisData = streamsClient.decodeData('Bib', event.Records.map(i => i.kinesis.data));
-
-    // Resolve the Promise and do something with the decoded data
-    decodedKinesisData
-      .then((result) => console.log('result:', result))
-      .catch((err) => console.log('rejected:', err));
+    exports.kinesisHandler(
+      event.Records,
+      { schema: 'Bib', apiUri: 'https://api.nypltech.org/api/v0.1/' },
+      context
+    );
   }
 };
