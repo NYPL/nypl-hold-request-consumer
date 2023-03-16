3b95bafe8354be098dc81d0ffcf743728255c3b3
danamansana <danielappel@nypl.org>
2023-03-16 16:12:29 -0400

    Add terraform deployment

---
+++ provisioning/resources.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+terraform {
+  # Use s3 to store terraform state
+  backend "s3" {
+    bucket  = "nypl-travis-builds-production"
+    key     = "hold-request-consumer-terraform-state"
+    region  = "us-east-1"
+  }
+}
+
+module "base" {
+  source = "../base"
+
+  environment = "production"
+
+  vpc_config = {
+    subnet_ids         = ["subnet-59bcdd03", "subnet-5deecd15"]
+    security_group_ids = ["sg-116eeb60"]
+  }
+}
